677d3185747df4ba652e4fe368ea5c881ccfe0f5
clemence dodeler <clemence.dodeler@gmail.com>
2020-01-09 12:09:25 +0100

    ajout de style Homepage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,18 +13,7 @@ class MainController extends AbstractController
      */
     public function main()
     {
-        return $this->render('main/index.html.twig', [
-
-
-        ]);
+        return $this->render('main/home.html.twig');
     }
-    /**
-     * @Route("/home", name="home")
-     */
-    public function home()
-    {
-        return $this->render('main/home.html.twig' );
-    }
-
 
 }
