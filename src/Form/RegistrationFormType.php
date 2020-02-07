ae3db3791c21cf833a3315598e91598b20437a8b
binblink <benjaminsotier@gmail.com>
2020-02-07 09:10:28 +0100

    modifs admin+log

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,6 +17,10 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('adresse')
+            ->add('tel')
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
